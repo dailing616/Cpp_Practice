f110c5de0fefb42e4e16ebc472e18a489da54909
lingdai <32312431+dailing616@users.noreply.github.com>
2019-08-04 08:30:30 -0500

    Add files via upload

---
+++ data_type.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade = 'A';
+    string phrase = "Papadox Dai";
+    int age = 24;
+    double gpa = 3.67;
+    bool isMale = true;
+
+    return 0;
+}
+
+#cout = console out
+#endl = end line
