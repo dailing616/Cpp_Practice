b8ab4178e47c6ef82e83347a3d4877f40c6e83ea
lingdai <32312431+dailing616@users.noreply.github.com>
2019-08-04 09:26:09 -0500

    Add files via upload

---
+++ user_input.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+    /* int age;//*/
+    /* cout << "Enter your age: ";//*/
+    /* cin, use >> instead of << //*/
+    /* cin >> age;//*/
+
+    /*cout << "You are " << age << " years old." << endl;//*/
+
+    string name;
+    getline(cin, name);
+    cout << "Hello " << name << endl;
+
+    return 0;
+}
