0b5bfa9fdd6ad91cf62ba919e2c5b9645097a248
lingdai <32312431+dailing616@users.noreply.github.com>
2019-08-04 14:40:15 -0500

    Add files via upload

---
+++ for_loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/* switch statement//*/
+
+
+int main()
+{
+    for(int i = 1; i<= 5; i++){
+        cout << i << endl;
+    }
+
+    return 0;
+}
+
