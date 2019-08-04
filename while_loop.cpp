b1b30a90011f522d710c9fd15c5a1e752ed34ab6
lingdai <32312431+dailing616@users.noreply.github.com>
2019-08-04 14:36:05 -0500

    Add files via upload

---
+++ while_loop.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/* switch statement//*/
+
+
+int main()
+{
+    int index = 1;
+
+    while (index <= 5){
+        cout << index << endl;
+        index++;
+    }
+
+    do{
+            cout << index << endl;
+        index++;
+    }while(index <= 10);
+
+    return 0;
+}
+
